a80c008fd2a282aa519e4bd4c7df1e6d83e828ca
augustbrown <august.brown@24g.com>
2026-08-13 19:02:37 -0400

    removing raw plugin code

--- plugins/friends-plugin/src/index.ts
+++
@@ -1,1 +0,0 @@
-export const FRIENDS_PLUGIN_META = { id: 'friends', version: '0.1.0' } as const;
--- plugins/friends-plugin/src/lib/client/mutual-friends-view.tsx
+++
@@ -1,9 +0,0 @@
-import { FriendsOfUserListView } from './friends-of-user-list-view.js';
-
-export interface MutualFriendsViewProps {
-  readonly userId: string;
-}
-
-export const MutualFriendsView = ({ userId }: MutualFriendsViewProps) => (
-  <FriendsOfUserListView path={`/friends/of/${userId}/mutual`} emptyMessage="No mutual friends" />
-);
